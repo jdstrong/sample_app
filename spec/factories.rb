e084c06a635963cba1090f7b0a6a42ba2d331d88
John David Strong <john.d.strong@etherpros.com>
2012-07-12 16:33:00 -0500

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "John David Strong"
+    email    "john.d.strong@etherpros.com"
+    password "123234"
+    password_confirmation "123234"
+  end
+end
